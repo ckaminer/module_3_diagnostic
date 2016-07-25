82eae5dde5481d03af85705deca50f9c3681b0d7
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-25 10:19:55 -0600

    finish writing user story test for first story begin implementation test calls in postman

--- spec/features/search_by_only_zip_spec.rb
+++ spec/features/search_by_only_zip_spec.rb
@@ -1,13 +1,20 @@
 require "rails_helper"
 
 RSpec.feature "user searches stations by only zip code" do
-  describe "they enter a valid zip code"
+  describe "they enter a valid zip code" do
     scenario "they see a list of the 10 closest stations within 6 miles sorted by distance" do
         visit "/"
-        fill_in "q", with: "80203"
+        fill_in "zip", with: "80203"
+        click_on "Locate"
 
         expect(current_path).to eq "/search?zip=80203"
-        
+        expect(page).to have_content "Station Name"
+        expect(page).to have_content "Fuel Type"
+        expect(page).to have_content "Distance"
+        expect(page).to have_content "Address"
+        expect(page).to have_content "Access Times"
+        expect(page).to_not have_content "Biodiesel"
+        expect(page).to_not have_content "Compressed Natural Gas"
     end
   end
 end
